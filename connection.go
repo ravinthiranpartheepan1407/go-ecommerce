5ccb580edd06cfc496ce3e0adf9acd58b531f8ad
surenravi1407 <surenravi1407@gmail.com>
2022-07-03 17:33:31 +0300

    connectionTest.go

--- connection.go
+++ connection.go
@@ -5,4 +5,16 @@ import(
 	"log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"github.com/ravinthiranpartheepan1407/go-ecommerce"
 )
+
+funct GetDB() *gorm.DB{
+	dbConfig := connection.DbConfig()
+	status:= fmt.Sprintf("host=%s, port=%s, password=%s, user=%s, dbname=%s, sslmode=%s", dbConfig.host, dbConfig.port, dbConfig.password, dbConfig.user, dbConfig.dbname, dbConfig.sslmode)
+	db, err := gorm.open(postgres.Open(status), &gorm.Config{})
+	if err != nil{
+		fmt.Println("Falied to connect with postgres")
+	}
+
+	return db
+}
--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+import "github.com/joho/godotenv"
+
+import "github.com/ravinthiranpartheepan1407/go-ecommerce"
+
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil{
 		fmt.Println("Failed to load env")
 	}
+
+	connection.GetDB()
 }
